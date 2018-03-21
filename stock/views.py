420be46528089f98ef878f105c63750755be719f
vishalmanani <vishal@lintelindia.com>
2018-03-21 20:13:14 +0530

    add stock app

--- stock/views.py
+++ stock/views.py
@@ -24,9 +24,6 @@ class AuthToken(View):
         print("status_code====>", response.status_code)
         print("status_text====>", response.text)
 
-        json_token = json.dumps(response.text)
-        print(json_token)
-        print(json_token.Token)
         location_url = "https://eu-ext.linnworks.net//api/Locations/GetLocation"
         l_payload = {
             "pkStockLocationId": "63d2d7b6-ec58-4f2c-a55e-2f8be11ea296",
@@ -41,7 +38,7 @@ class AuthToken(View):
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
             'Referer': 'https://www.linnworks.net/',
             'Accept-Encoding': 'gzip,deflate',
-            'Authorization': json_token.Token,
+            'Authorization': token,
         }
         location = requests.post(location_url, data=json.dumps(l_payload), headers=headers)
         print("location_code===>", location.status_code)
