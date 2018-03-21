c860d9f6065b8e25d2ee9d1a2d6cd2f952bfc91f
vishalmanani <vishal@lintelindia.com>
2018-03-21 16:50:44 +0530

    add stock app

--- stock/views.py
+++ stock/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views import View
 import requests
-from django.http import JsonResponse
 
 
 class AuthToken(View):
@@ -14,26 +13,13 @@ class AuthToken(View):
 
         url = 'https://api.linnworks.net//api/Auth/AuthorizeByApplication'
         payload = {
-            'applicationId': '8d9172f1-9fe0-4394-95ee-25495e35d89f',
-            'applicationSecret': '8af5f1fb-b384-42da-86c8-667132cf2349',
-            'token': token
+            "applicationId": "65625950-d006-4e8f-a5a4-b3c8e1ca4cfe",
+            "applicationSecret": "8f0df2ed-9338-402b-9dd8-4b3efa560d0e",
+            "token": token,
         }
         response = requests.post(url, data=payload)
-        print("====>", response)
+
         print("status_code====>", response.status_code)
-        print("status_code====>", response.text)
-        print("====>", dir(response))
+        print("status_text====>", response.text)
 
         return render(request, self.template, locals())
-
-    def post(self, request):
-        url = 'https://api.linnworks.net//api/Auth/AuthorizeByApplication'
-        payload = {
-            'applicationId': '8d9172f1-9fe0-4394-95ee-25495e35d89f',
-            'applicationSecret': '8af5f1fb-b384-42da-86c8-667132cf2349',
-            'token': '1d8993875b980f035df7f71c6afac8eb'
-        }
-        data = requests.post(url, data=payload)
-        print("====>", data)
-        response = {'status': '200'}
-        return JsonResponse(response)
