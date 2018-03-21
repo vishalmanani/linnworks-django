91fa868824ab0aae46d0a7ba41ba2aabdb328f91
vishalmanani <vishal@lintelindia.com>
2018-03-21 14:59:19 +0530

    add stock app

--- stock/views.py
+++ stock/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+import requests
+from django.http import JsonResponse
 
 
 class AuthToken(View):
@@ -10,3 +12,15 @@ class AuthToken(View):
         token = request.GET.get('token', None)
         print("token=======>", token)
         return render(request, self.template, locals())
+
+    def post(self, request):
+        url = 'https://api.linnworks.net//api/Auth/AuthorizeByApplication'
+        payload = {
+            'applicationId': '8d9172f1-9fe0-4394-95ee-25495e35d89f',
+            'applicationSecret': '8af5f1fb-b384-42da-86c8-667132cf2349',
+            'token': '1d8993875b980f035df7f71c6afac8eb'
+        }
+        data = requests.post(url, data=payload)
+        print("====>", data)
+        response = {'status': '200'}
+        return JsonResponse(response)
