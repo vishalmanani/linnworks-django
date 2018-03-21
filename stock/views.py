8926d7326b095646f3cc675fc32880edd3389691
vishalmanani <vishal@lintelindia.com>
2018-03-21 15:05:21 +0530

    add stock app

--- stock/views.py
+++ stock/views.py
@@ -11,6 +11,19 @@ class AuthToken(View):
         print("call get method")
         token = request.GET.get('token', None)
         print("token=======>", token)
+
+        url = 'https://api.linnworks.net//api/Auth/AuthorizeByApplication'
+        payload = {
+            'applicationId': '8d9172f1-9fe0-4394-95ee-25495e35d89f',
+            'applicationSecret': '8af5f1fb-b384-42da-86c8-667132cf2349',
+            'token': token
+        }
+        response = requests.post(url, data=payload)
+        print("====>", response)
+        print("status_code====>", response.status_code)
+        print("status_code====>", response.text)
+        print("====>", dir(response))
+
         return render(request, self.template, locals())
 
     def post(self, request):
