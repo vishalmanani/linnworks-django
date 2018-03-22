8b98cd819e0e349ff0dc7edad2241b7be084efee
vishalmanani <vishal@lintelindia.com>
2018-03-22 11:57:30 +0530

    add stock app

--- stock/views.py
+++ stock/views.py
@@ -37,18 +37,18 @@ class TestApi(View):
     def get(self, request):
         main_token = Token.objects.last()
         print("main_token last===>", main_token.token)
-        location_url = "https://eu-ext.linnworks.net//api/Stock/GetStockConsumption"
-        l_payload = {
-            "stockItemId": "ceb6c502-54c4-4ca9-b9cb-5d1131a309da",
-            "locationId": "ed65daa9-2867-4d05-b30f-3868b469a7e6",
-            "startDate": "2018-02-19T16:57:06.0303122+00:00",
-            "endDate": "2018-02-19T16:57:06.0358278+00:00"
-        }
+        location_url = "https://eu-ext.linnworks.net//api/Inventory/GetPackageGroups"
+        # l_payload = {
+        #     "stockItemId": "ceb6c502-54c4-4ca9-b9cb-5d1131a309da",
+        #     "locationId": "ed65daa9-2867-4d05-b30f-3868b469a7e6",
+        #     "startDate": "2018-02-19T16:57:06.0303122+00:00",
+        #     "endDate": "2018-02-19T16:57:06.0358278+00:00"
+        # }
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
             'Authorization': main_token.token,
         }
-        location = requests.post(location_url, data=json.dumps(l_payload), headers=headers)
+        location = requests.post(location_url, headers=headers)
         print("location_code===>", location.status_code)
         print("location_text===>", location.text)
 
